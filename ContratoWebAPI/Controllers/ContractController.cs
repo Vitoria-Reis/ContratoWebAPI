e4d8ce088db37771f49092d19aac27c42ada80fa
Vitoria Reis <72982321+Vitoria-Reis@users.noreply.github.com>
2021-01-18 14:20:54 -0300

    perf: Update in the Context entity

--- ContratoWebAPI/Controllers/ContractController.cs
+++ ContratoWebAPI/Controllers/ContractController.cs
@@ -11,6 +11,12 @@ namespace ContratoWebAPI.Controllers
     [Route("v1/contracts")]
     public class ContractController : ControllerBase
     {
+        private DataContext _context;
+        
+        public ContractController(DataContext context) {
+        _context = context;
+        }
+
         [HttpGet]
         [Route("")]
         public async Task<ActionResult<List<Contract>>> Get([FromServices] DataContext context)
