5d3e97823b064cac0734cf41dc4ca713ac6edfe7
Vitoria Reis <72982321+Vitoria-Reis@users.noreply.github.com>
2021-01-18 13:49:11 -0300

    feat: Update in the Context entity

--- ContratoWebAPI/Controllers/ContractController.cs
+++ ContratoWebAPI/Controllers/ContractController.cs
@@ -1,1 +1,22 @@
-Controller
+using Microsoft.AspNetCore.Mvc;
+using ContratoWebAPI.Data;
+using ContratoWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ContratoWebAPI.Controllers
+{
+    [ApiController]
+    [Route("v1/contracts")]
+    public class ContractController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<Contract>>> Get([FromServices] DataContext context)
+        {
+            var contracts = await context.Contracts.ToListAsync();
+            return contracts;
+        }
+    }
+}
