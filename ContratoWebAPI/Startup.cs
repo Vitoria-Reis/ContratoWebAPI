ce2abb61278f90dd3821511454918b109f6894a2
Vitoria Reis <72982321+Vitoria-Reis@users.noreply.github.com>
2021-01-20 10:56:40 -0300

    feat: Adiction FeatureManager in Statup.cs

--- ContratoWebAPI/Startup.cs
+++ ContratoWebAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using ContratoWebAPI.Data;
+using Microsoft.FeatureManagement;
 
 namespace ContratoWebAPI
 {
@@ -31,6 +32,7 @@ namespace ContratoWebAPI
             services.AddScoped<DataContext, DataContext>();
             services.AddControllers();
             services.AddMemoryCache();
+            services.AddFeatureManagement();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
